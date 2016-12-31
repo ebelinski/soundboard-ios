2747176b99995014e6a22ad26d194d5f7aa76913
Eugene Belinski <hhws@ep.local>
2016-12-31 15:02:00 -0600

    reindented to 2 spaces

--- Soundboard/ViewController.swift
+++ Soundboard/ViewController.swift
@@ -1,10 +1,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+  
+  override func viewDidAppear(_ animated: Bool) {
+    super.viewDidAppear(animated)
+    
+    
+  }
+  
 }
