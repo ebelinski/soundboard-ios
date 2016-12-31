3c631602613ec9394f422ba6d4c850f0c3a2898d
Eugene Belinski <hhws@ep.local>
2016-12-31 15:29:43 -0600

    play sound even on silent mode

--- Soundboard/ViewController.swift
+++ Soundboard/ViewController.swift
@@ -5,6 +5,16 @@ class ViewController: UIViewController {
   
   var currentSound: AVAudioPlayer?
   
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    do {
+      try AVAudioSession.sharedInstance().setCategory(AVAudioSessionCategoryPlayback)
+    } catch {
+      print(error.localizedDescription)
+    }
+  }
+  
   @IBAction func didTapPlayButton(_ sender: Any) {
     if let soundURL = Bundle.main.url(forResource: "catapult-the-propaganda", withExtension: "mp3") {
       do {
