e8f779e43deb178027868822db91a473575675b6
Eugene Belinski <hhws@ep.local>
2016-12-31 16:02:28 -0600

    moved audio playing code to AudioManager

--- Soundboard/AudioManager.swift
+++ Soundboard/AudioManager.swift
@@ -1,7 +1,30 @@
 import UIKit
+import AVFoundation
 
 struct AudioManager {
   
-  static let sharedInstance = AudioManager()
+  static var sharedInstance = AudioManager()
+  
+  var currentPlayer: AVAudioPlayer?
+  
+  init() {
+    do {
+      try AVAudioSession.sharedInstance().setCategory(AVAudioSessionCategoryPlayback)
+    } catch {
+      print(error.localizedDescription)
+    }
+  }
+  
+  mutating func playSound() {
+    if let sound = NSDataAsset(name: "catapult-the-propaganda") {
+      do {
+        let player = try AVAudioPlayer(data: sound.data, fileTypeHint: AVFileTypeMPEGLayer3)
+        currentPlayer = player
+        player.play()
+      } catch {
+        print(error.localizedDescription)
+      }
+    }
+  }
   
 }
--- Soundboard/ViewController.swift
+++ Soundboard/ViewController.swift
@@ -1,30 +1,9 @@
 import UIKit
-import AVFoundation
 
 class ViewController: UIViewController {
   
-  var currentPlayer: AVAudioPlayer?
-  
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    
-    do {
-      try AVAudioSession.sharedInstance().setCategory(AVAudioSessionCategoryPlayback)
-    } catch {
-      print(error.localizedDescription)
-    }
-  }
-  
   @IBAction func didTapPlayButton(_ sender: Any) {
-    if let sound = NSDataAsset(name: "catapult-the-propaganda") {
-      do {
-        let player = try AVAudioPlayer(data: sound.data, fileTypeHint: AVFileTypeMPEGLayer3)
-        currentPlayer = player
-        player.play()
-      } catch {
-        print(error.localizedDescription)
-      }
-    }
+    AudioManager.sharedInstance.playSound()
   }
   
 }
