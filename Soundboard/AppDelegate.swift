f496debe0ae0bab6316827243992ae67151ebd94
Eugene Belinski <hhws@ep.local>
2016-12-31 10:31:36 -0600

    initial cleanup

--- Soundboard/AppDelegate.swift
+++ Soundboard/AppDelegate.swift
@@ -1,18 +1,9 @@
-//
-//  AppDelegate.swift
-//  Soundboard
-//
-//  Created by Eugeniu Belinski on 12/30/16.
-//  Copyright © 2016 Eugeniu Belinski. All rights reserved.
-//
-
 import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
@@ -41,6 +32,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
 
-
 }
-
--- Soundboard/ViewController.swift
+++ Soundboard/ViewController.swift
@@ -1,11 +1,3 @@
-//
-//  ViewController.swift
-//  Soundboard
-//
-//  Created by Eugeniu Belinski on 12/30/16.
-//  Copyright © 2016 Eugeniu Belinski. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
@@ -15,11 +7,4 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
-
