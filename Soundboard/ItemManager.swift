8c352ff7fc0dc1859cf36f8bb3cb2290cd20c5ae
Eugene Belinski <hhws@ep.local>
2016-12-31 18:55:25 -0600

    used divider | instead of , in csv

--- Soundboard/ItemManager.swift
+++ Soundboard/ItemManager.swift
@@ -19,7 +19,7 @@ struct ItemManager {
       let contents = try String(contentsOfFile: pathForCSV)
       let rawItems = contents.characters.split{$0 == "\n"}.map(String.init)
       for line in rawItems {
-        let rawItem = line.characters.split{$0 == ","}.map(String.init)
+        let rawItem = line.characters.split{$0 == "|"}.map(String.init)
         if rawItem.count == 3 {
           items.append(Item(rawItem: rawItem))
         } else {
