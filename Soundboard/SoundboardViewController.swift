ad875688e5dc043745f6d99b8a9112f58b397253
Eugene Belinski <hhws@ep.local>
2016-12-31 19:00:02 -0600

    removed useless code

--- Soundboard/SoundboardViewController.swift
+++ Soundboard/SoundboardViewController.swift
@@ -2,11 +2,6 @@ import UIKit
 
 class SoundboardViewController: UITableViewController {
   
-  override func viewDidAppear(_ animated: Bool) {
-    super.viewDidAppear(animated)
-    print(tableView.numberOfRows(inSection: 0))
-  }
-  
   override func numberOfSections(in tableView: UITableView) -> Int {
     return 1
   }
@@ -23,10 +18,6 @@ class SoundboardViewController: UITableViewController {
   
   override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
     AudioManager.sharedInstance.play(sound: ItemManager.sharedInstance.items[indexPath.row].audioFileName)
-  }
-  
-  @IBAction func didTapPlayButton(_ sender: Any) {
-    AudioManager.sharedInstance.play(sound: ItemManager.sharedInstance.items[0].audioFileName)
   }
   
 }
