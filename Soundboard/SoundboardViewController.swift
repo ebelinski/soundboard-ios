089dc98ca266f66a6f290eeea2418e0a0dba6f69
Eugene Belinski <hhws@ep.local>
2017-01-01 16:04:53 -0600

    created filteredItems and sourceItems

--- Soundboard/SoundboardViewController.swift
+++ Soundboard/SoundboardViewController.swift
@@ -2,22 +2,31 @@ import UIKit
 
 class SoundboardViewController: UITableViewController {
   
+  let items = ItemManager.sharedInstance.items
+  var filteredItems = [Item]()
+  var showFilteredItems = false
+  var sourceItems: [Item] {
+    get {
+      return showFilteredItems ? filteredItems : items
+    }
+  }
+  
   override func numberOfSections(in tableView: UITableView) -> Int {
     return 1
   }
   
   override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return ItemManager.sharedInstance.items.count
+    return sourceItems.count
   }
   
   override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
     let cell = UITableViewCell()
-    cell.textLabel?.text = ItemManager.sharedInstance.items[indexPath.row].name
+    cell.textLabel?.text = sourceItems[indexPath.row].name
     return cell
   }
   
   override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-    AudioManager.sharedInstance.play(sound: ItemManager.sharedInstance.items[indexPath.row].name)
+    AudioManager.sharedInstance.play(sound: sourceItems[indexPath.row].name)
   }
   
 }
