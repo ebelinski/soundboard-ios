78832e3e80ff9409016208114985f9dfaa9c11ed
Eugene Belinski <hhws@ep.local>
2017-01-02 17:58:23 -0600

    added app color variables

--- Soundboard/Constants.swift
+++ Soundboard/Constants.swift
@@ -1,1 +1,7 @@
+import UIKit
+
 let itemsCSVFileName = "items"
+
+let appColorDark = UIColor(red: 0.176, green: 0.294, blue: 0.376, alpha: 1) /* #2d4b60 */
+let appColorMedium = UIColor(red: 0.278, green: 0.522, blue: 0.576, alpha: 1) /* #478593 */
+let appColorLight = UIColor(red: 0.573, green: 0.894, blue: 0.918, alpha: 1) /* #92e4ea */
