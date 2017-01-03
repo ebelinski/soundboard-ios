14e2160ee30bd4d2ef817d1ac5be868308e1b81b
Eugene Belinski <hhws@ep.local>
2017-01-03 16:20:43 -0600

    updated light color, made it darker

--- Soundboard/Constants.swift
+++ Soundboard/Constants.swift
@@ -4,4 +4,4 @@ let itemsCSVFileName = "items"
 
 let appColorDark = UIColor(red: 0.176, green: 0.294, blue: 0.376, alpha: 1) /* #2d4b60 */
 let appColorMedium = UIColor(red: 0.278, green: 0.522, blue: 0.576, alpha: 1) /* #478593 */
-let appColorLight = UIColor(red: 0.573, green: 0.894, blue: 0.918, alpha: 1) /* #92e4ea */
+let appColorLight = UIColor(red: 0.373, green: 0.651, blue: 0.714, alpha: 1) /* #5fa6b6 */
