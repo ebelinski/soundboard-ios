e1cdcb2422796864fe746e3bae68f4b6553253f1
Eugene Belinski <hhws@ep.local>
2017-01-02 18:01:02 -0600

    set some cell colors

--- Soundboard/ItemTableViewCell.swift
+++ Soundboard/ItemTableViewCell.swift
@@ -5,4 +5,12 @@ class ItemTableViewCell: UITableViewCell {
   @IBOutlet weak var nameLabel: UILabel!
   @IBOutlet weak var descriptionLabel: UILabel!
   
+  override func awakeFromNib() {
+    super.awakeFromNib()
+    
+    backgroundColor = appColorMedium
+    nameLabel.textColor = UIColor.white
+    descriptionLabel.textColor = UIColor.white
+  }
+  
 }
