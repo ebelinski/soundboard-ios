9bbf50d996871e6ff3fc46709395d0a2525ad7e1
Eugene Belinski <hhws@ep.local>
2017-01-03 18:26:09 -0600

    used ItemCollectionViewCell

--- Soundboard/SoundboardViewController.swift
+++ Soundboard/SoundboardViewController.swift
@@ -1,30 +1,25 @@
 import UIKit
 
-private let reuseIdentifier = "Cell"
-
 class SoundboardViewController: UICollectionViewController {
   
   override func viewDidLoad() {
     super.viewDidLoad()
     
-    self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+    collectionView?.register(UINib(nibName: "ItemCollectionViewCell", bundle: Bundle.main), forCellWithReuseIdentifier: "itemcell")
   }
-  
-  // MARK: UICollectionViewDataSource
   
   override func numberOfSections(in collectionView: UICollectionView) -> Int {
-    // #warning Incomplete implementation, return the number of sections
-    return 0
+    return 1
   }
   
-  
   override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-    // #warning Incomplete implementation, return the number of items
-    return 0
+    return 1
   }
   
   override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-    let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
+    guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "itemcell", for: indexPath) as? ItemCollectionViewCell else {
+      return UICollectionViewCell()
+    }
     
     return cell
   }
