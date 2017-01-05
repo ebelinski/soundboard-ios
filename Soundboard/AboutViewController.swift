b003402384e3612463209370f305eb5a6f30eb5c
Eugene Belinski <hhws@ep.local>
2017-01-04 19:26:30 -0600

    added functionality to buttons

--- Soundboard/AboutViewController.swift
+++ Soundboard/AboutViewController.swift
@@ -22,7 +22,7 @@ class AboutViewController: UIViewController {
     var contentHeight: CGFloat = spaceHorizontal
     
     let informationPart1 = UILabel(frame: CGRect(x: spaceVertical, y: contentHeight, width: contentWidth, height: 0))
-    informationPart1.text = "Soundboard for TYT was created by Eugene Belinski. It's available for iOS and Android! Please contact me if you find any bugs, want me to add a new sound, or if you have any comment or suggestion."
+    informationPart1.text = "Soundboard for TYT was created by Eugene Belinski and is available for iOS and Android. Please contact me if you find any bugs, want me to add a new sound, or if you have any comment or suggestion."
     setLabelStandardProperties(label: informationPart1)
     scrollView?.addSubview(informationPart1)
     contentHeight += informationPart1.frame.height
@@ -31,6 +31,7 @@ class AboutViewController: UIViewController {
     
     let buttonTwitter = UIButton(frame: CGRect(x: spaceVertical, y: contentHeight, width: 0, height: 0))
     buttonTwitter.setTitle("@EugeneBelinski", for: .normal)
+    buttonTwitter.addTarget(self, action: #selector(AboutViewController.didTapButtonTwitter(sender:)), for: .touchUpInside)
     setButtonStandardProperties(button: buttonTwitter)
     scrollView.addSubview(buttonTwitter)
     self.buttonTwitter = buttonTwitter
@@ -40,6 +41,7 @@ class AboutViewController: UIViewController {
     
     let buttonWebsite = UIButton(frame: CGRect(x: spaceVertical, y: contentHeight, width: 0, height: 0))
     buttonWebsite.setTitle("My Website", for: .normal)
+    buttonWebsite.addTarget(self, action: #selector(AboutViewController.didTapButtonWebsite(sender:)), for: .touchUpInside)
     setButtonStandardProperties(button: buttonWebsite)
     scrollView.addSubview(buttonWebsite)
     self.buttonWebsite = buttonWebsite
@@ -75,6 +77,14 @@ class AboutViewController: UIViewController {
     button.frame.size = CGSize(width: button.frame.size.width + 25, height: button.frame.size.height)
     button.frame.origin = CGPoint(x: screenWidth / 2 - button.frame.width / 2, y: button.frame.origin.y)
     button.layer.cornerRadius = button.frame.height / 4
+  }
+  
+  func didTapButtonTwitter(sender: AnyObject) {
+    UIApplication.shared.open(URL(string: "https://twitter.com/EugeneBelinski")!, options: [:], completionHandler: nil)
+  }
+  
+  func didTapButtonWebsite(sender: AnyObject) {
+    UIApplication.shared.open(URL(string: "http://ebelinski.com")!, options: [:], completionHandler: nil)
   }
   
 }
