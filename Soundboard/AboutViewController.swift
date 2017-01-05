4c989eb6e765b099fd84240abb033c914776632e
Eugene Belinski <hhws@ep.local>
2017-01-04 19:18:24 -0600

    added label and buttons to about page

--- Soundboard/AboutViewController.swift
+++ Soundboard/AboutViewController.swift
@@ -2,8 +2,79 @@ import UIKit
 
 class AboutViewController: UIViewController {
   
-  override func viewDidLoad() {
-    super.viewDidLoad()
+  @IBOutlet weak var scrollView: UIScrollView!
+  
+  let spaceVertical: CGFloat = 20
+  let spaceHorizontal: CGFloat = 30
+  let contentWidth: CGFloat = screenWidth - 20 * 2
+  let fontSize: CGFloat = 28
+  
+  var buttonTwitter: UIButton?
+  var buttonWebsite: UIButton?
+  
+  override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
+    view.backgroundColor = appColorMedium
+    setUpScrollViewContent()
+  }
+  
+  private func setUpScrollViewContent() {
+    var contentHeight: CGFloat = spaceHorizontal
+    
+    let informationPart1 = UILabel(frame: CGRect(x: spaceVertical, y: contentHeight, width: contentWidth, height: 0))
+    informationPart1.text = "Soundboard for TYT was created by Eugene Belinski. It's available for iOS and Android! Please contact me if you find any bugs, want me to add a new sound, or if you have any comment or suggestion."
+    setLabelStandardProperties(label: informationPart1)
+    scrollView?.addSubview(informationPart1)
+    contentHeight += informationPart1.frame.height
+    
+    contentHeight += spaceHorizontal
+    
+    let buttonTwitter = UIButton(frame: CGRect(x: spaceVertical, y: contentHeight, width: 0, height: 0))
+    buttonTwitter.setTitle("@EugeneBelinski", for: .normal)
+    setButtonStandardProperties(button: buttonTwitter)
+    scrollView.addSubview(buttonTwitter)
+    self.buttonTwitter = buttonTwitter
+    contentHeight += buttonTwitter.frame.height
+    
+    contentHeight += spaceHorizontal
+    
+    let buttonWebsite = UIButton(frame: CGRect(x: spaceVertical, y: contentHeight, width: 0, height: 0))
+    buttonWebsite.setTitle("My Website", for: .normal)
+    setButtonStandardProperties(button: buttonWebsite)
+    scrollView.addSubview(buttonWebsite)
+    self.buttonWebsite = buttonWebsite
+    contentHeight += buttonWebsite.frame.height
+    
+    contentHeight += spaceHorizontal
+    
+    let informationPart2 = UILabel(frame: CGRect(x: spaceVertical, y: contentHeight, width: contentWidth, height: 0))
+    informationPart2.text = "If you liked this app, please consider making a donation to Wolf PAC or becoming a TYT member."
+    setLabelStandardProperties(label: informationPart2)
+    scrollView.addSubview(informationPart2)
+    contentHeight += informationPart2.frame.height
+    
+    contentHeight += spaceHorizontal
+    
+    scrollView.contentSize = CGSize(
+      width: contentWidth,
+      height: contentHeight)
+  }
+  
+  private func setLabelStandardProperties(label: UILabel) {
+    label.font = label.font.withSize(fontSize)
+    label.numberOfLines = 0
+    label.textColor = appColorText
+    label.sizeToFit()
+  }
+  
+  private func setButtonStandardProperties(button: UIButton) {
+    button.titleLabel?.font = button.titleLabel?.font.withSize(fontSize)
+    button.setTitleColor(appColorDark, for: .normal)
+    button.backgroundColor = UIColor.white
+    button.sizeToFit()
+    button.frame.size = CGSize(width: button.frame.size.width + 25, height: button.frame.size.height)
+    button.frame.origin = CGPoint(x: screenWidth / 2 - button.frame.width / 2, y: button.frame.origin.y)
+    button.layer.cornerRadius = button.frame.height / 4
   }
   
 }
